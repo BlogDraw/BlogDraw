6c7018c5280f822f7606952bc4fee29911a8c5af
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2018-07-18 23:27:37 +0100

    Update index.php
    Added comments for code documentation.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * index.php - this is the default page called by users accessing the site.  It passes all of the work on to the relevant functions in the functions.php file, and initialises the chosen template.
+**/
 	require_once ('./functions.php');
 	require_once ('./template/' . TEMPLATE . '/index.php');
 ?>
